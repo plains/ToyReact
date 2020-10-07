efbf626539aed05235e93ca7d4dd28531ad7bd57
plains <zhangzhongyuan@qutoutiao.net>
2020-10-07 21:49:48 +0800

    feat: class 02

--- src/toy-react.js
+++ src/toy-react.js
@@ -1,8 +1,13 @@
+const isObject = obj => {
+    return obj !== null && typeof obj === "object"
+}
+
 export class Component {
     constructor() {
         this.props = Object.create(null)
         this.children = []
-        this._root = undefined;
+        this._root = null
+        this._range = null
     }
     get root(){
         if (!this._root) {
@@ -14,26 +19,79 @@ export class Component {
         this.props[p] = value
     }
     appendChild(component) {
-        this.children.push(component);
+        this.children.push(component)
     }
     render() {}
+    _renderToDom(range) {
+        this._range = range
+        this.render()._renderToDom(range)
+    }
+    reRender() {
+        this._range.deleteContents()
+        this._renderToDom(this._range)
+    }
+    setState(newState) {
+        if (!isObject(this.state) || this.state === undefined) {
+            this.state = newState
+            this.reRender()
+            return
+        }
+        let merge = (oldState, newState) => {
+            for (let p in newState) {
+                if (isObject(newState[p])) {
+                    if (oldState[p] === undefined || oldState[p] === null) {
+                        oldState[p] = 
+                        (newState[p] instanceof Array && []) || 
+                        (newState[p] instanceof Object && {})
+                    }
+                    merge(oldState[p], newState[p])
+                } else {
+                    oldState[p] = newState[p]
+                }
+            }
+        }
+        merge(this.state, newState)
+        this.reRender()
+    }
 }
 
 class ElementWrapper {
     constructor(type) {
         this.root = document.createElement(type)
+        this._range = null
     }
     setAttribute(p, value) {
-        return this.root.setAttribute(p, value)
+        if (p.match(/on([\s\S]+)$/)) {
+            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
+        } else if(p.toLowerCase() === "classname") {
+            this.root.setAttribute("class", value)
+        } else {
+            this.root.setAttribute(p, value)
+        }
     }
     appendChild(component) {
-        return this.root.appendChild(component.root)
+        let range = document.createRange()
+        range.setStart(this.root, this.root.childNodes.length)
+        range.setEnd(this.root, this.root.childNodes.length)
+        component._renderToDom(range)
+    }
+    _renderToDom(range) {
+        range.deleteContents()
+        range.insertNode(this.root)
+    }
+    reRender() {
+        this._range.deleteContents()
+        this._renderToDom(this._range)
     }
 }
 
 class TextNodeWrapper {
     constructor(text) {
         this.root = document.createTextNode(text)
+    }
+    _renderToDom(range) {
+        range.deleteContents()
+        range.insertNode(this.root)
     }
 }
 
@@ -50,12 +108,12 @@ export function createElement(type, attrs, ...children) {
     
     let insertChildren = (children) => {
         for(let child of children){
-            if (typeof child === "string") {
-                child = new TextNodeWrapper(child)
-            }
             if (typeof child === "object" && child instanceof Array) {
                 insertChildren(child)
+            } else if(child instanceof ElementWrapper || child instanceof Component) {
+                node.appendChild(child)
             } else {
+                child = new TextNodeWrapper(child)
                 node.appendChild(child)
             }
         }
@@ -65,6 +123,10 @@ export function createElement(type, attrs, ...children) {
     return node
 }
 
-export function render(node, container) {
-    container.appendChild(node.root)
+export function render(component, parentElement) {
+    let range = document.createRange()
+    range.setStart(parentElement, 0)
+    range.setEnd(parentElement, parentElement.childNodes.length)
+    range.deleteContents()
+    component._renderToDom(range)
 }
